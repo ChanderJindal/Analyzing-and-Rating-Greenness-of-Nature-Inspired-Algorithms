dd139dabb72fad7208dabc981fab6ee153fa1721
cj-kid <chanderjindal2@gmail.com>
2022-11-20 15:36:23 +0530

    try 2

---
+++ my_tries/temp.py
@@ -0,0 +1,7 @@
+NIA_Val_Dict = {0:'Cuckoo Search',1:'Fire Fly',2:'Bat',3:'Hybrid Bat',4:'Self Adaptive Bat',5:'Hybrid Self Adaptive Bat'}
+
+NIA_lst = [CucKoo_pbounds, FireFly_pbounds, BAT_pbounds, HBA_pbounds, SABA_pbounds, HSABA_pbounds]
+
+Save_dict_fxn = [Save_cuckoo_dict, Save_firefly_dict, Save_bat_dict, Save_hba_dict, Save_sba_dict, Save_hsaba_dict]
+
+NIA_Score_fxn = [Score_Cuckoo, Score_Firefly, Score_Bat, Score_HBA, Score_SBA, Score_HBA]
