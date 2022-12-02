2c7f317761075f5a5fe41fc81d946a23abbb09e5
cj-kid <chanderjindal2@gmail.com>
2022-12-03 01:18:25 +0530

    checkpoint

--- my_tries/temp.py
+++ my_tries/temp.py
@@ -1,7 +1,1 @@
-NIA_Val_Dict = {0:'Cuckoo Search',1:'Fire Fly',2:'Bat',3:'Hybrid Bat',4:'Self Adaptive Bat',5:'Hybrid Self Adaptive Bat'}
-
-NIA_lst = [CucKoo_pbounds, FireFly_pbounds, BAT_pbounds, HBA_pbounds, SABA_pbounds, HSABA_pbounds]
-
-Save_dict_fxn = [Save_cuckoo_dict, Save_firefly_dict, Save_bat_dict, Save_hba_dict, Save_sba_dict, Save_hsaba_dict]
-
-NIA_Score_fxn = [Score_Cuckoo, Score_Firefly, Score_Bat, Score_HBA, Score_SBA, Score_HBA]
+f'{population_size}_{c1}_{c2}_{w}_{min_velocity}_{max_velocity}_{repair}_{n_estimator}_{criterion}_{max_feature}'
